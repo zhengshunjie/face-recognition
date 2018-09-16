bd048823fabb12272bf21371eb5435bf55582dd1
colabin <632683582@qq.com>
2018-09-16 21:52:28 +0800

    Create tf_records.py
    generate tf_records

---
+++ utils/tf_records.py
@@ -0,0 +1,63 @@
+"""Training a face recognizer with TensorFlow using softmax cross entropy loss
+"""
+# MIT License
+#
+# Copyright (c) 2016 David Sandberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from datetime import datetime
+import os.path
+import time
+import sys
+import random
+import tensorflow as tf
+import numpy as np
+import importlib
+import facenet
+import tensorflow as tf
+from PIL import Image
+
+def main():
+
+    train_filename = './train.tfrecords'
+    writer = tf.python_io.TFRecordWriter(train_filename)
+
+    dataset = facenet.get_dataset("~/data")
+    image_list, label_list = facenet.get_image_paths_and_labels(dataset)
+    assert(len(image_list)==len(label_list))
+    for i in range(0,len(image_list)):
+        image = image_list[i]
+        label = label_list[i]
+        img = image.resize((image.shape[0], image.shape[1]));
+        img_raw=img.tobytes()
+        example = tf.train.Example(features=tf.train.Features(feature={
+            "label": tf.train.Feature(int64_list=tf.train.Int64List(value=label)),
+            'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=img_raw))
+        }))
+        writer.write(example.SerializeToString())
+    writer.close()
+
+
+if __name__ == '__main__':
+    main()

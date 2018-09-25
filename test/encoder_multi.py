a2163478f7d46f98920e557dfe5946100936c251
colabin <632683582@qq.com>
2018-09-25 20:21:57 +0800

    Update encoder_multi.py

--- test/encoder_multi.py
+++ test/encoder_multi.py
@@ -9,7 +9,7 @@ facenet_model_checkpoint = os.path.dirname(__file__) + '/checkpoint_multi'
 
 class FaceEncoder:
     def __init__(self):
-        self.sess = tf.Session()
+        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True))
         with self.sess.as_default():
             load_model(facenet_model_checkpoint)
             #print 'load model is done'
@@ -21,7 +21,6 @@ class FaceEncoder:
         images_placeholder = tf.get_default_graph().get_tensor_by_name("img_inputs_1:0")
         #images_placeholder2 = tf.get_default_graph().get_tensor_by_name("img_inputs:0")
         embeddings = tf.get_default_graph().get_tensor_by_name("tower_0/resnet_v1_50_1/E_BN2/Identity:0")
-        print(embeddings)
         #phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
         dropout_rate_placeholder = self.sess.graph.get_tensor_by_name("dropout_rate:0")
         # Run forward pass to calculate embeddings

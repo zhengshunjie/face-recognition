48be439b4b22260717250ef122287d1858c3e9a6
colabin <632683582@qq.com>
2018-10-12 17:47:49 +0800

    Update preload_utils.py

--- utils/preload_utils.py
+++ utils/preload_utils.py
@@ -2,6 +2,21 @@
 from tensorflow.python import pywrap_tensorflow
 import os
 
+#打印变量
+tf.contrib.framework.get_variables_to_restore()
+tf.all_variables()
+tf.trainable_varialbes()
+
+#得到该网络中，所有可以加载的参数
+variables = tf.contrib.framework.get_variables_to_restore()
+#删除output层中的参数
+variables_to_resotre = [v for v in varialbes if v.name.split('/')[0]!='output']
+#构建这部分参数的saver
+saver = tf.train.Saver(variables_to_restore)
+saver.restore(sess,'model.ckpt')
+
+
+
 #这种办法无法打印出img_placeholder这种tensor
 checkpoint_path = os.path.join( "checkpoint-00454721")
 reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
@@ -10,6 +25,8 @@ for key in var_to_shape_map:
         print("tensor_name: ", key)
         
 
+        
+        
 #初始化相关变量
 uninitialized_vars = []
 for var in tf.global_variables():
